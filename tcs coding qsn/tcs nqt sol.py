a2ed5706cd2a89bccb7a3b4fbd20f60156cc7c95
Sudhir swain <70561891+sudhir192311@users.noreply.github.com>
2021-12-27 19:33:59 +0530

    first commit

---
+++ tcs coding qsn/tcs nqt sol.py
@@ -0,0 +1,29 @@
+Given an input string, you have to encrypt the string in such a manner that ‘a’ becomes ‘z’, ‘z’ becomes ‘a’ and accordingly.
+
+ 
+
+Input format
+The input is a lower case string
+
+ 
+
+Output format
+Encrypted lower case string
+
+ 
+
+Example
+Input:
+programmings
+
+Output:
+kiltiznnrmt
+slo:=
+s=input("input the string->")
+res=" "
+for i in range(len(s)):
+    res+=chr(ord('a')-ord(s[i])+ord('z'))
+
+
+
+print(res)
